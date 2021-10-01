9a1dfd7639e082aa694445c29499c6558ba31916
iaredave <gouletdavid95@gmail.com>
2021-10-01 02:32:18 -0400

    Add register, login activities, integrate with firebase auth for custom sign in

---
+++ app/src/main/java/com/example/videogameexplorer/MainActivityLogin.java
@@ -0,0 +1,40 @@
+package com.example.videogameexplorer;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class MainActivityLogin extends AppCompatActivity {
+
+    Button btnLogOut;
+    FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnLogOut = findViewById(R.id.btnLogout);
+        mAuth = FirebaseAuth.getInstance();
+
+        btnLogOut.setOnClickListener(view ->{
+            mAuth.signOut();
+            startActivity(new Intent(MainActivityLogin.this, LoginActivity.class));
+        });
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser user = mAuth.getCurrentUser();
+        if (user == null){
+            startActivity(new Intent(MainActivityLogin.this, LoginActivity.class));
+        }
+    }
+}
